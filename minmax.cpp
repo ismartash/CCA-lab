a709a8762b26a2043236eb341013e874dba40a83
unknown <ismartash>
2024-10-05 22:06:08 +0530

    commited

---
+++ minmax.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+void minmaxi(int arr[], int start, int end, int &mini, int &maxi) {
+    if (start == end) {
+        mini=arr[start];
+        maxi=arr[end];
+        return;
+    }
+
+    if (start + 1 == end) {
+        if (arr[start] < arr[end]) {
+            mini= arr[start];
+            maxi= arr[end];
+        } else {
+            mini= arr[end];
+            maxi= arr[start];
+        }
+        return;
+    }
+
+    int mid = (start + end) / 2;
+    int lmin, lmax, rmin, rmax;
+
+    minmaxi(arr, start, mid, lmin, lmax);
+    minmaxi(arr, mid + 1, end, rmin, rmax);
+
+    mini= min(lmin, rmin);
+    maxi= max(lmax, rmax);
+}
+
+int main() {
+    int n;
+    cout<<"enter no of elements"<<endl;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cout<<"enter elements"<<endl;
+        cin>>arr[i];
+    }
+    int mini= INT_MAX;
+    int maxi= INT_MIN;
+
+    minmaxi(arr, 0, n - 1, mini, maxi);
+
+    cout << "minimum= " << mini<< endl;
+    cout << "maximum= " << maxi<< endl;
+
+    return 0;
+}
---
+++ quicksort.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+int partition(int arr[],int first,int last,int n)
+{
+    int pivot=arr[last];
+    int j=first;
+    int i=first-1;
+     for(int j=first;j<last;j++)
+     {
+        if(arr[j]<pivot)
+        {
+            i++;
+            swap(arr[i],arr[j]);
+        }
+     }
+     swap(arr[i+1],arr[last]);
+     return i+1;
+}
+void quicksort(int arr[],int first,int last,int n)
+{
+    if(first>=last)
+    {
+        return ;
+    }
+    else
+    {
+    int ash=partition(arr,first,last,n);
+    quicksort(arr,first,ash-1,n);
+    quicksort(arr,ash+1,last,n);
+    }
+}
+int main()
+{
+    int n;
+    cout<<"enter no of elements"<<endl;
+    cin>>n;
+    int first=0;
+    int last=n-1;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cout<<"enter elements"<<endl;
+        cin>>arr[i];
+    }
+    quicksort(arr,first,last,n);
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }   
+    return 0;
+}
